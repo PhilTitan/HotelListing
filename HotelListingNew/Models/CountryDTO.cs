9f738cdbeeff25b27e7b2084b4b63cf1cecfed6b
Phil Peters <phil.peters@centralreach.com>
2021-06-19 16:36:30 -0400

    Added Database, AutoMapper, Unit of Work and DTOs

---
+++ HotelListingNew/Models/CountryDTO.cs
@@ -0,0 +1,26 @@
+﻿using HotelListingNew.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListingNew.Models
+{
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country Name is too long")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short Country Name is too long")]
+        public string ShortName { get; set; }
+    }
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+        public IList<Hotel> Hotels { get; set; }
+    }
+
+    
+}
--- HotelListingNew/Startup.cs
+++ HotelListingNew/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListingNew.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +13,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using HotelListingNew.Configurations;
 
 namespace HotelListingNew
 {
@@ -26,8 +30,9 @@ namespace HotelListingNew
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
 
             services.AddCors(o =>
             {
@@ -40,10 +45,13 @@ namespace HotelListingNew
                 });
             });
 
+            services.AddAutoMapper(typeof(MapperInitializer));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Listing", Version = "v1" });
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
